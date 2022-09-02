8dc567c6f7b1edd8b4de3120832714a920e5d9d6
Darya <HavrylenkoIT@gmail.com>
2022-09-02 23:57:46 +0300

    edit task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,44 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+const onPlay = function () {
+  const timeupdate = {
+    duration: 61.857,
+    percent: 0.049,
+    seconds: 3.034,
+  };
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(timeupdate));
+};
+onPlay();
+player.on('play', onPlay);
+
+iframe.addEventListener('timeupdate', onPlay);
+
+let timeVideo;
+player.setCurrentTime(localStorage.getItem(STORAGE_KEY)).then(function(seconds) {
+    seconds = localStorage.setItem(STORAGE_KEY);
+    
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            timeVideo <= 0;
+            break;
+
+        default:
+            // console.error('error');
+            break;
+    }
+});
