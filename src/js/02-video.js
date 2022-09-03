c25d1ff5c6d67b5efc90f25a35f52867a49a06f9
Darya <HavrylenkoIT@gmail.com>
2022-09-03 16:25:32 +0300

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,39 +6,18 @@ const STORAGE_KEY = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
-
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
-
-const onPlay = function () {
-  const timeupdate = {
-    duration: 61.857,
-    percent: 0.049,
-    seconds: 3.034,
-  };
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(timeupdate));
+const onPlay = function (data = {}) {
+  console.log(data.seconds);
+ localStorage.setItem(STORAGE_KEY, data.seconds);
+   
 };
-onPlay();
-player.on('play', onPlay);
 
-iframe.addEventListener('timeupdate', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000));
 
-let timeVideo;
-player.setCurrentTime(localStorage.getItem(STORAGE_KEY)).then(function(seconds) {
-    seconds = localStorage.setItem(STORAGE_KEY);
-    
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            timeVideo <= 0;
-            break;
+const currentTime = localStorage.getItem('videoplayer-current-time');
+console.log(currentTime);
+if (currentTime) {
+    player.setCurrentTime(currentTime);
+}
 
-        default:
-            // console.error('error');
-            break;
-    }
-});
+  
